2d055c9839011879ca20c605fa5e702f2f1455dd
poorlearninghack <saloniyk26@gmail.com>
2025-06-03 16:19:50 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hail all the poor people who are striving for better future!!!!!!!!!</p>
+      <p>People might hold you by hands, push you backwards to not let you feel comfortable or worthy enough to pursue a good career, But never loose the real charm of your dreams. And somehow you have deal with both, securing good future while dealing with poverty. It was never supposed to be one after the another.</p>
+      <p>I believe in you.</p>
+    </div>
+  );
+}
